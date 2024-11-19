81bc9f5fff5cdac00696ab7839a09d53a2419d71
saleha qareen <qare0001@algonquinlive.com>
2024-11-19 14:28:09 -0500

    indented

--- footer.php
+++ footer.php
@@ -1,20 +1,19 @@
-
 <!DOCTYPE html>
 <html lang="en">
-    
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    
-    <body>
-    
-    
-    </html>
-    <footer>
-            <p>&copy; <?php echo date("Y"); ?> Recipe Manager by Saleha and Zahra. All Rights Reserved.</p>
-    </footer>
-    
-    </body>
-    
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+
+</html>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Recipe Manager by Saleha and Zahra. All Rights Reserved.</p>
+</footer>
+
+</body>
+
 </html>

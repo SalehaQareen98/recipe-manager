6bd321d2077ae35796467ca4e0edfca4101ed413
SalehaQareen98 <salehaqareen@gmail.com>
2024-11-22 01:55:50 -0500

    Added styles for new recipe page

--- new.php
+++ new.php
@@ -9,66 +9,74 @@ if (!isset($_SESSION['user_id'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="style.css" />
-  <title>Create New Recipe</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Recipe</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  
-<?php include 'header.php'; ?>
 
-<div id="content">
+    <?php include 'header.php'; ?>
 
-  <a class="back-link" href="<?php echo 'home.php'; ?>">Home page</a>
+    <div class="wrapper-container create-page">
+        <div class="container">
+            <div class="form-box">
+                <h1 class="form-title">Create New Recipe</h1>
 
-  <div class="new-recipe">
-    <h1>Create New Recipe</h1>
+                <form action="create.php" method="POST">
 
-    <form action="create.php" method="POST">
-      <dl>
-        <dt>Recipe Title</dt>
-        <dd><input type="text" name="title" required /></dd>
-      </dl>
-      <dl>
-        <dt>Time to Cook</dt>
-        <dd><input type="text" name="time_to_cook" required /></dd>
-      </dl>
-      <dl>
-        <dt>Is Vegetarian</dt>
-        <dd>
-          <select name="vegetarian" required>
-            <option value="1">Yes</option>
-            <option value="0">No</option>
-          </select>
-        </dd>
-      </dl>
-      <dl>
-        <dt>Ingredients</dt>
-        <dd><textarea name="ingredients" rows="5" required></textarea></dd>
-      </dl>
-      <dl>
-        <dt>Directions</dt>
-        <dd><textarea name="directions" rows="10" required></textarea></dd>
-      </dl>
-      <dl>
-        <dt>Recipe Type</dt>
-        <dd>
-          <select name="type" required>
-            <option value="Appetizer">Appetizer</option>
-            <option value="Main Course">Main Course</option>
-            <option value="Dessert">Dessert</option>
-            <option value="Drinks">Drinks</option>
-          </select>
-        </dd>
-      </dl>
-      <div id="operations">
-        <input type="submit" value="Create Recipe" />
-      </div>
-    </form>
-  </div>
+                    <h2>Recipe Details</h2>
 
-</div>
+                    <div class="form-group">
+                        <label class="label" for="title">Recipe Title</label>
+                        <input class="input" type="text" id="title" name="title" required />
+                    </div>
 
-<?php include 'footer.php'; 
-?>
+                    <div class="form-group">
+                        <label class="label" for="time_to_cook">Time to Cook</label>
+                        <input class="input" type="text" id="time_to_cook" name="time_to_cook" required />
+                    </div>
+
+                    <div class="form-group">
+                        <label class="label" for="vegetarian">Is Vegetarian</label>
+                        <select class="select" id="vegetarian" name="vegetarian" required>
+                            <option value="1">Yes</option>
+                            <option value="0">No</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="label" for="ingredients">Ingredients</label>
+                        <textarea class="textbox" id="ingredients" name="ingredients" rows="5" required></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="label" for="directions">Directions</label>
+                        <textarea class="textbox" id="directions" name="directions" rows="10" required></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="label" for="type">Recipe Type</label>
+                        <select class="select" id="type" name="type" required>
+                            <option value="Appetizer">Appetizer</option>
+                            <option value="Main Course">Main Course</option>
+                            <option value="Dessert">Dessert</option>
+                            <option value="Drinks">Drinks</option>
+                        </select>
+                    </div>
+
+                    <div id="operations">
+                        <button class="signup-button" type="submit">Create Recipe</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+
 </body>
 </html>

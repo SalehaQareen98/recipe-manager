85b95495540bd7d00736ea954dead59a4c287e4f
SalehaQareen98 <salehaqareen@gmail.com>
2024-11-21 22:56:58 -0500

    fixed footer and some styles

--- footer.php
+++ footer.php
@@ -4,16 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipe Manager</title>
 </head>
 
 <body>
-
-
-</html>
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Recipe Manager by Saleha and Zahra. All Rights Reserved.</p>
-</footer>
-
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Recipe Manager by Saleha and Zahra. All Rights Reserved.</p>
+    </footer>
 </body>
 
 </html>

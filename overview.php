7daccfebf02661847a67e899e8c2abad5fe288f2
Zahra Pyarali <zahrapyarli19@gmail.com>
2024-11-21 22:26:54 -0500

    created overview page

---
+++ overview.php
@@ -0,0 +1,18 @@
+<?php include 'header.php'; ?>
+
+<div class="overview-container">
+    <img src="images/title-img.jpg" alt="Recipe Manager" class="banner-image">
+    <h2>Welcome to Recipe Manager!</h2>
+    <p class="description">
+        Recipe Manager is your personalized digital cookbook, where you can create, organize, and store your favorite recipes. Whether you are a home cook, a professional chef, or just someone who loves to explore the world of culinary arts, our platform makes it simple and fun to manage your recipes.
+    </p>
+    <p class="description">
+        Start creating your own recipes, categorize them by type, and access them anytime, anywhere. Enjoy the convenience of storing your recipes in one place and sharing them with friends and family!
+    </p>
+    <div>
+        <a href="registration.html" class="cta-button">Sign Up</a>
+        <a href="index.php" class="cta-button secondary-button">Login</a>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>

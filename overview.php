7662865b443aa552b25f949f33aedd2013765406
Zahra Pyarali <zahrapyarli19@gmail.com>
2024-11-21 22:31:31 -0500

    removed css classes

--- overview.php
+++ overview.php
@@ -1,17 +1,17 @@
 <?php include 'header.php'; ?>
 
-<div class="overview-container">
-    <img src="images/title-img.jpg" alt="Recipe Manager" class="banner-image">
+<div>
+    <img src="images/title-img.jpg" alt="Recipe Manager">
     <h2>Welcome to Recipe Manager!</h2>
-    <p class="description">
+    <p>
         Recipe Manager is your personalized digital cookbook, where you can create, organize, and store your favorite recipes. Whether you are a home cook, a professional chef, or just someone who loves to explore the world of culinary arts, our platform makes it simple and fun to manage your recipes.
     </p>
-    <p class="description">
+    <p>
         Start creating your own recipes, categorize them by type, and access them anytime, anywhere!
     </p>
     <div>
-        <a href="registration.html" class="cta-button">Sign Up</a>
-        <a href="index.php" class="cta-button secondary-button">Login</a>
+        <a href="registration.html">Sign Up</a>
+        <a href="index.php">Login</a>
     </div>
 </div>
 
